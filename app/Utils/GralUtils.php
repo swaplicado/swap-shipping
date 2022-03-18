dd4bbb279bb9de895eb017ef633669e9579f8622
Edwin Carmona <eedwincarmona@gmail.com>
2022-03-18 09:35:13 -0600

    Update GralUtils.php
    -Formatter

--- app/Utils/GralUtils.php
+++ app/Utils/GralUtils.php
@@ -1,49 +1,55 @@
 <?php namespace App\Utils;
 
 class GralUtils {
-    
-    public static function arrayToObject($array) {
-        $obj = new \stdClass;
-        foreach($array as $k => $v) {
-           if(strlen($k)) {
-              if(is_array($v)) {
-                 $obj->{$k} = GralUtils::arrayToObject($v); //RECURSION
-              } else {
-                 $obj->{$k} = $v;
-              }
-           }
-        }
-        return $obj;
-    }
 
-   public static function getShipType($idState, $idMun, $idZipCode){
+   public static function arrayToObject($array)
+   {
+      $obj = new \stdClass;
+      foreach ($array as $k => $v) {
+         if (strlen($k)) {
+               if (is_array($v)) {
+                  $obj->{$k} = GralUtils::arrayToObject($v); //RECURSION
+               } else {
+                  $obj->{$k} = $v;
+               }
+         }
+      }
+      return $obj;
+   }
+
+   public static function getShipType($idState, $idMun, $idZipCode)
+   {
       $shipType = '';
 
       $state = \DB::table('f_local_locations')
-                     ->where('state_id', $idState)
-                     ->first();
-      
-      if(!is_null($state)){
-         $shipType = "L";    
-      }else{
+                  ->where('state_id', $idState)
+                  ->first();
+
+      if (!is_null($state)) {
+         $shipType = "L";
+      }
+      else {
          $municipalitie = \DB::table('f_local_locations')
                               ->where('municipality_id', $idMun)
                               ->first();
 
-         if(!is_null($municipalitie)){
-            $shipType = "L";    
-         }else{
-            $zip_code = \DB::table('f_local_locations')
-                           ->where('zip_code', $idZipCode)
-                           ->first();
-   
-            if(!is_null($zip_code)){
+         if (!is_null($municipalitie)) {
                $shipType = "L";
-            }else{
-               $shipType = "F";
-            }
+         }
+         else {
+               $zip_code = \DB::table('f_local_locations')
+                              ->where('zip_code', $idZipCode)
+                              ->first();
+
+               if (!is_null($zip_code)) {
+                  $shipType = "L";
+               }
+               else {
+                  $shipType = "F";
+               }
          }
       }
-      return $shipType;          
+      return $shipType;
    }
+
 }
